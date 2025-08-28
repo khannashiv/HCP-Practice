4ee5bec1bfe1c50d15f7341c16326f31d8c80b58
Shiv <khannashiv94@gmail.com>
2025-08-28 08:56:18 +0530

    test

--- ec2.tf
+++ ec2.tf
@@ -2,3 +2,15 @@ resource "aws_instance" "web" {
   ami           = "ami-0360c520857e3138f"
   instance_type = "t2.micro"
 }
+
+resource "aws_security_group" "demo" {
+  description = "Allow SSH"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ing" {
+  security_group_id = aws_instance.web.id
+  ip_protocol = "tcp"
+  to_port = 22
+  from_port = 22
+  cidr_ipv4 = "0.0.0.0/0"
+}
